2eb91fc74b60acc58828e185a0e681f1d68c64d3
pixaliser <77645626+pixaliser@users.noreply.github.com>
2021-01-19 09:49:30 +0200

    Update helpers.h

--- helpers.h
+++ helpers.h
@@ -1,9 +1,9 @@
-#ifndef _V_SH_HELPERS_H_
-#define _V_SH_HELPERS_H_
+#ifndef _RI_SH_HELPERS_H_
+#define _RI_SH_HELPERS_H_
 
 int count_char(char *str, char c);
 int strvlen(char **strv);
 
 char **strtostrv(char *str, char sep);
 
-#endif //_V_SH_HELPERS_H_
+#endif //_RI_SH_HELPERS_H_
